a4be642f5586be554187fd07dbde08beafa137eb
Sunny-jinn <rlawlsdn316@gmail.com>
2022-08-01 08:50:08 +0900

    Frontend 연결 및 backend 수정

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,54 +1,78 @@
-const express=require('express')
-const Post=require('./../models/post')
-const router=express.Router()
+const express = require("express");
+const Post = require("./../models/post");
+const router = express.Router();
 
+router.get("/new", (req, res) => {
+  res.render("posts/new", { post: new Post() });
+});
 
-router.get('/new',(req, res) =>{
-    res.render('posts/new', {post : new Post() })
-})
+router.get("/", async (req, res) => {
+  const posts = await Post.find().sort({
+    createdAt: "desc", //최신순으로 정렬
+  });
+  res.send({ posts: posts });
+});
 
-router.get('/edit/:id', async (req, res) => {
-    const post = await Post.findById(req.params.id)
-    res.render('posts/edit', {post:post})
-})
-router.get('/:slug', async (req, res) => {
-    const post = await Post.findOne({ slug : req.params.slug })
-    if (post == null ) res.redirect('/')
-    res.render('posts/show', {post: post})
-})
+router.get("/test", (req, res) => {
+  res.send({ message: "자고싶다" });
+});
 
-router.post('/', async (req, res, next)=>{
-    req.post = new Post()
-    next()
-       
-}, savePostAndRedirect('new'))
+router.get("/edit/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("posts/edit", { post: post });
+});
 
-router.put('/:id', async (req, res, next)=>{
-    req.post = await Post.findById(req.params.id)
-    next()
-       
-}, savePostAndRedirect('edit'))
+router.get("/:slug", async (req, res) => {
+  const post = await Post.findOne({ slug: req.params.slug });
+  if (post == null) res.redirect("/");
+  res.render("posts/show", { post: post });
+});
 
-router.delete('/:id', async (req, res) => {
-    await Post.findByIdAndDelete(req.params.id)
-    res.redirect('/')
-})
+router.post(
+  "/",
+  async (req, res, next) => {
+    req.post = new Post();
+    next();
+  },
+  savePostAndRedirect("new")
+);
 
-function savePostAndRedirect(path){
-    return async (req, res) => {
-        let post = req.post
-        post.title = req.body.title
-        post.description = req.body.description
-        post.markdown = req.body.markdown
-        try{
-          post = await post.save()
-          res.redirect(`/posts/${post.slug}`)
-        } catch(e){
-         res.render(`posts/${path}`, { post : post })
-        }
-        
+router.post(
+  "/new",
+  async (req, res, next) => {
+    req.post = new Post();
+    next();
+  },
+  savePostAndRedirect("new")
+);
+
+router.put(
+  "/:id",
+  async (req, res, next) => {
+    req.post = await Post.findById(req.params.id);
+    next();
+  },
+  savePostAndRedirect("edit")
+);
+
+router.delete("/delete", async (req, res) => {
+  await Post.findByIdAndDelete(req.body.id);
+  res.redirect("/");
+});
+
+function savePostAndRedirect(path) {
+  return async (req, res) => {
+    let post = req.post;
+    post.title = req.body.post.title;
+    post.description = req.body.post.description;
+    post.markdown = req.body.post.markdown;
+    try {
+      post = await post.save();
+      res.redirect(`/posts/${post.slug}`);
+    } catch (e) {
+      res.render(`posts/${path}`, { post: post });
     }
+  };
 }
 
-
-module.exports=router
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,31 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const Post = require('./models/post')
-const postRouter=require('./routes/posts')
-const app=express()
-const methodOverride=require('method-override')
+const express = require("express");
+const mongoose = require("mongoose");
+const Post = require("./models/post");
+const postRouter = require("./routes/posts");
+const app = express();
+const methodOverride = require("method-override");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-mongoose.connect('mongodb://localhost/blog',{useNewUrlParser : true, useUnifiedTopology : true})
-app.set('view engine','ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
+mongoose.connect("mongodb://localhost/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(cors());
 
-app.get('/', async (req, res)=> {
-    const posts= await Post.find().sort({
-        createdAt: 'desc' //최신순으로 정렬
-    })
-    res.render('posts/index',{posts:posts})
-})
+app.get("/", async (req, res) => {
+  const posts = await Post.find().sort({
+    createdAt: "desc", //최신순으로 정렬
+  });
+  res.render("posts/index", { posts: posts });
+});
 
-app.use('/posts',postRouter)
+app.use("/posts", postRouter);
 
-app.listen(5000)
+app.listen(5000);
